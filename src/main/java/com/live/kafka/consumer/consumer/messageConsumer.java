e87f0487416e3cd8dfe9dff22e073e4cd6441d9b
André Demori <andremdemori@gmail.com>
2024-12-06 11:09:07 -0300

    update

--- src/main/java/com/live/kafka/consumer/consumer/messageConsumer.java
+++ src/main/java/com/live/kafka/consumer/consumer/messageConsumer.java
@@ -2,7 +2,6 @@ package com.live.kafka.consumer.consumer;
 
 import com.live.kafka.consumer.DTO.MessageDTO;
 import com.live.kafka.consumer.config.KafkaConfig;
-import com.live.kafka.consumer.service.SiplomService;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,11 +13,9 @@ public class messageConsumer {
 
     private static final Logger logger = LoggerFactory.getLogger(messageConsumer.class);
     private final KafkaConfig kafkaConfig;
-    private final SiplomService siplomService;
 
-    public messageConsumer(KafkaConfig kafkaConfig, SiplomService siplomService) {
+    public messageConsumer(KafkaConfig kafkaConfig) {
         this.kafkaConfig = kafkaConfig;
-        this.siplomService = siplomService;
     }
 
     @KafkaListener(topics = "#{kafkaConfig.topicName}",
@@ -29,11 +26,5 @@ public class messageConsumer {
         logger.info("Received Message in partition " + record.partition());
         logger.info("Received Message: " + record.value());
 
-        MessageDTO message = record.value();
-        try {
-            siplomService.sendToSiplom(message);
-        } catch (Exception e) {
-            logger.error("Error processing message: ", e);
-        }
     }
 }
